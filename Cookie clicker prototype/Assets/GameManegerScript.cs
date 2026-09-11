76a6939b6f90b8e6367d15103c4cc01392d54a9e
Nahuel Johansson Videla <Nahuel.Johansson-Videla@elev.lbs.ga.se>
2026-09-11 10:17:17 +0200

    cookire game
    progga grandma logic plus cost multiplier, animations.

---
+++ Cookie clicker prototype/Assets/GameManegerScript.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using TMPro;
+using System;
+public class GameManegerScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+        [SerializeField] TextMeshProUGUI cookietext;
+        [SerializeField] int Cookies;
+
+        [SerializeField] Animator canvasAnimator;
+
+
+    [SerializeField] int grandmaCount;      
+    [SerializeField] int GrandmaCost;
+    [SerializeField] TextMeshProUGUI Grandma_upgrade_cost;  
+    // int cookie;
+
+    float timer;
+
+  private void Start()
+    {
+        Grandma_upgrade_cost.text = GrandmaCost.ToString();
+    }
+
+
+
+    private void Update()
+    {
+        if (grandmaCount > 0)
+        {
+            if (timer >= 1)
+            {
+                timer = 0;
+                Cookies += grandmaCount;   // en cookie per grandma, varje sekund
+                cookietext.text = Cookies.ToString();
+            }
+            timer += Time.deltaTime;
+        }
+    }
+
+
+
+  
+
+
+
+
+
+
+
+
+
+    public void CookieClicker()
+    {
+            Cookies++;
+            cookietext.text = Cookies.ToString();
+            Debug.Log(Cookies);
+            canvasAnimator.SetTrigger("Cookie_Shake");
+
+
+    }
+
+    public void BuyGrandma()
+    {
+        if (Cookies >= GrandmaCost)
+        {
+            Cookies -= GrandmaCost;       
+            grandmaCount++;               
+            GrandmaCost = Mathf.RoundToInt(GrandmaCost * 1.15f);
+
+            cookietext.text = Cookies.ToString();
+            Grandma_upgrade_cost.text = GrandmaCost.ToString();  
+        }
+    }
+
+
+
+
+
+
+}
+
+   
+    
